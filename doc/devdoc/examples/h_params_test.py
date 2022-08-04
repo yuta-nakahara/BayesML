465eb803a291e5e8f4ea73d9ff601aae95bf4075
yuta-nakahara <92844770+yuta-nakahara@users.noreply.github.com>
2022-08-05 02:27:24 +0900

    revise h_params of categorical

--- doc/devdoc/examples/h_params_test.py
+++ doc/devdoc/examples/h_params_test.py
@@ -1,14 +1,44 @@
-from bayesml import bernoulli
+from bayesml import categorical as bayesml_model
+import numpy as np
 
-model = bernoulli.GenModel(h_alpha=0.5,h_beta=0.3)
-params = model.get_params()
-model.save_params('tmp.pkl')
-print(params)
+h0_params = {'h0_alpha_vec':np.ones(5)}
 
-model_2 = bernoulli.GenModel(theta=0.9,h_alpha=1.0,h_beta=1.0)
-print(model_2.get_params())
-# model_2.set_h_params(*h_params.values())
-# print(model_2.get_h_params())
+print('Gen to Learn 1')
+model = bayesml_model.GenModel()
+print(model.get_h_params())
+model.save_h_params('tmp.pkl')
 
-model_2.load_params('tmp.pkl')
-print(model_2.get_params())
+model_2 = bayesml_model.LearnModel(**h0_params)
+print(model_2.get_h0_params())
+model_2.load_h0_params('tmp.pkl')
+print(model_2.get_h0_params())
+
+print('Gen to Learn 2')
+model = bayesml_model.GenModel()
+print(model.get_h_params())
+model.save_h_params('tmp.pkl')
+
+model_2 = bayesml_model.LearnModel(**h0_params)
+print(model_2.get_hn_params())
+model_2.load_hn_params('tmp.pkl')
+print(model_2.get_hn_params())
+
+print('Learn to Gen 1')
+model_2 = bayesml_model.LearnModel(**h0_params)
+print(model_2.get_h0_params())
+model_2.save_h0_params('tmp.pkl')
+
+model = bayesml_model.GenModel()
+print(model.get_h_params())
+model.load_h_params('tmp.pkl')
+print(model.get_h_params())
+
+print('Learn to Gen 2')
+model_2 = bayesml_model.LearnModel(**h0_params)
+print(model_2.get_hn_params())
+model_2.save_hn_params('tmp.pkl')
+
+model = bayesml_model.GenModel()
+print(model.get_h_params())
+model.load_h_params('tmp.pkl')
+print(model.get_h_params())
