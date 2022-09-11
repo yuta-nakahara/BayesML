849c69d10af7edc3372f6d712b6743e9546ffd42
NJ-local <jun.b.nishikawa@gmail.com>
2022-09-11 23:15:00 +0900

    add params for data generation without hyper params

--- bayesml/_check.py
+++ bayesml/_check.py
@@ -51,6 +51,12 @@ def nonneg_int_vec(val,val_name,exception_class):
             return val
     raise(exception_class(val_name + " must be a 1-dimensional numpy.ndarray whose dtype is int. Its values must be non-negative (including 0)."))
 
+def nonneg_float_vec(val,val_name,exception_class):
+    if type(val) is np.ndarray:
+        if np.issubdtype(val.dtype,np.floating) and val.ndim == 1 and np.all(val>=0):
+            return val
+    raise(exception_class(val_name + " must be a 1-dimensional numpy.ndarray whose dtype is float. Its values must be non-negative (including 0)."))
+
 def int_of_01(val,val_name,exception_class):
     if np.issubdtype(type(val),np.integer):
         if val == 0 or val ==1:
@@ -171,13 +177,14 @@ def float_vecs(val,val_name,exception_class):
             return val
     raise(exception_class(val_name + " must be a numpy.ndarray whose ndim >= 1."))
 
-def float_vec_sum_1(val,val_name,exception_class):
+def float_vec_sum_1(val,val_name,exception_class,ndim=1,sum_axis=0):
     if type(val) is np.ndarray:
-        if np.issubdtype(val.dtype,np.integer) and val.ndim == 1 and abs(val.sum() - 1.) <= _EPSILON:
+        sum_val = np.sum(val, axis=sum_axis)
+        if np.issubdtype(val.dtype,np.integer) and val.ndim == ndim and abs(sum_val.sum() - np.prod(sum_val.shape)) <= _EPSILON:
             return val.astype(float)
-        if np.issubdtype(val.dtype,np.floating) and val.ndim == 1 and abs(val.sum() - 1.) <= _EPSILON:
+        if np.issubdtype(val.dtype,np.floating) and val.ndim == ndim and abs(sum_val.sum() - np.prod(sum_val.shape)) <= _EPSILON:
             return val
-    raise(exception_class(val_name + " must be a 1-dimensional numpy.ndarray, and the sum of its elements must equal to 1."))
+    raise(exception_class(val_name + f" must be a {ndim}-dimensional numpy.ndarray, and the sum of its elements must equal to 1."))
 
 def int_(val,val_name,exception_class):   
     if np.issubdtype(type(val),np.integer):
--- bayesml/hiddenmarkovautoregressive/_hiddenmarkovautoregressive.py
+++ bayesml/hiddenmarkovautoregressive/_hiddenmarkovautoregressive.py
@@ -6,23 +6,43 @@
 
 import numpy as np
 
+from .. import _check
+from .._exceptions import ParameterFormatError
+
 class GenModel:
     def __init__(
         self, 
         *, 
-        K: int = None, 
-        n: int = None, 
-        z_n: np.ndarray = None, 
-        pi: np.ndarray = None, 
-        a_jk: np.ndarray = None, 
-        A: np.ndarray = None, 
-        d: int = None, 
-        x_n_da: np.ndarray = None, 
-        theta_k: np.ndarray = None, 
-        theta: np.ndarray = None, 
-        x_n: float = None, 
-        tau_k: float = None, 
-        tau: np.ndarray = None
+        degree: int = 1, 
+        K: int = 2, 
+        n: int = 1, 
+        z_vec: np.ndarray = None, 
+        pi_vec: np.ndarray = None, 
+        A_matrix: np.ndarray = None, 
+        x_n_vec: np.ndarray = None, 
+        theta_matrix: np.ndarray = None, 
+        x_n: float = 1, 
+        tau_vec: np.ndarray = None
     ):
-        pass
         # TODO: get default values
+        # TODO: 境界値テスト
+
+        # [input variables]
+        self.degree = _check.pos_int(degree, "degree", ParameterFormatError)
+        self.K = _check.pos_int(K, "K", ParameterFormatError)
+        self.n = _check.pos_int(n, "n", ParameterFormatError)
+        self.x_n = _check.float_(x_n, "x_n", ParameterFormatError)
+        # [check values or set default values]
+        self.z_vec = np.array([1,] + [0,]*self.K) \
+            if z_vec is None else _check.onehot_vec(z_vec, "z_vec", ParameterFormatError)
+        self.pi_vec = np.ones(self.K) / self.K  \
+            if pi_vec is None else _check.nonneg_float_vec(pi_vec, "pi_vec", ParameterFormatError)
+        self.A_matrix = np.broadcast_to(np.reshape(self.pi_vec, [self.K,1]), [self.K,]*2) \
+            if A_matrix is None else _check.float_vec_sum_1(A_matrix, "A_matrix", ParameterFormatError, ndim=2, sum_axis=0)
+        self.x_n_vec = np.ones(self.K + 1) \
+            if x_n_vec is None else _check.float_vec(x_n_vec, "x_n_vec", ParameterFormatError)
+        self.theta_matrix = np.zeros(self.K,self.degree+1) \
+            if theta_matrix is None else _check.float_vecs(theta_matrix, "theta_matrix", ParameterFormatError)
+        self.tau_vec = self.pi_vec.copy() \
+            if tau_vec is None else _check.float_vec(tau_vec, "tau_vec", ParameterFormatError)
+
