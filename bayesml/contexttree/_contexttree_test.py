d89d42b8ed3f1cb50eb4f78b3f6e64d1be0b869b
yuta-nakahara <92844770+yuta-nakahara@users.noreply.github.com>
2022-11-20 11:31:29 +0900

    Add gen and visualize

---
+++ bayesml/contexttree/_contexttree_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import bayesml.contexttree as model
+
+gen_model = model.GenModel(c_k=2,c_d_max=3,h_g=0.75)
+# print(gen_model.get_params())
+gen_model.gen_params()
+gen_model.visualize_model()
+# params = gen_model.get_params()
+
+# gen_model = model.GenModel(2,c_d_max=3)
+# gen_model.set_params(root=params['root'])
+# gen_model.visualize_model(filename='tmp2.gv')
