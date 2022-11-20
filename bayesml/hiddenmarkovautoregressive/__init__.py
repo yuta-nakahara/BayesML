86eb7dd39fb1fd81ddb438cde57fb30f30196e25
Yuta Nakahara <92844770+yuta-nakahara@users.noreply.github.com>
2022-11-20 23:00:11 +0900

    Delete __init__.py

--- bayesml/hiddenmarkovautoregressive/__init__.py
+++
@@ -1,9 +0,0 @@
-# Document Author
-# Koki Kazama <kokikazama@aoni.waseda.jp>
-# Jun Nishikawa <Jun.B.Nishikawa@gmail.com>
-
-from ._hiddenmarkovautoregressive import GenModel
-# from ._hiddenmarkovautoregressive import LearnModel
-
-# __all__ = ["GenModel","LearnModel"]
-__all__ = ["GenModel"]
